77a40ee84523c334fe6ffd6ebbe0ec60596602ba
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:11:46 +0530

    Create Counting Sequences.cpp

---
+++ Additional Problems/Counting Sequences.cpp
@@ -0,0 +1,99 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+const int n=1e6+5;
+int power(int a,int b){
+    if(b==0)return 1;
+    if(a==0)return 0;
+    if(a==1)return 1;
+    int ans=1;
+    a=a%mod;
+    while(b>0){
+        if(b&1){
+            ans=ans*a%mod;
+        }
+        b>>=1;
+        a=(a*a)%mod;
+    }
+    return ans;
+}
+int fact[n]; int inv[n];
+void once(){
+    fact[0]=1;
+    REP(i,1,n){
+        fact[i]=fact[i-1]*i%mod;
+    }
+    inv[0]=1;
+    REP(i,1,n){
+        inv[i]=power(fact[i],mod-2)%mod;
+    }
+}
+int ncr(int n,int r){
+    int ans=fact[n]%mod;
+    ans*=inv[r]%mod;
+    ans%=mod;
+    ans*=inv[n-r]%mod;
+    ans%=mod;
+    return ans;
+}
+int addmod(int x,int y){
+    return (x+y)%mod;
+}
+ 
+int multmod(int x,int y){
+    return (x*y)%mod;
+}
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+ 
+void solve(int xyz)
+{
+    int n,k;cin>>n>>k;int ans=0;
+    REP(i,0,k){
+        if(i&1){
+              ans=addmod(ans,mod-multmod(ncr(k,i),power(k-i,n)));
+        }
+        else{
+        ans=addmod(ans,multmod(ncr(k,i),power(k-i,n)));}
+    }
+    cout<<ans;
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+once();
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
