75e459578e6ae9cd4343b8cdee970a0bb3dd003a
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 15:39:35 +0530

    Create Substring Distribution.cpp

---
+++ String Algorithms/Substring Distribution.cpp
@@ -0,0 +1,163 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+// int addmod(int x,int y){
+//     return (x+y)%mod;
+// }
+ 
+// int multmod(int x,int y){
+//     return (x*y)%mod;
+// }
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+#define MAX_N 100010
+ 
+char T[MAX_N];
+int n;
+int RA[MAX_N], tempRA[MAX_N];
+int SA[MAX_N], tempSA[MAX_N];
+int c[MAX_N];
+int LCP[MAX_N];
+int PLCP[MAX_N], Phi[MAX_N];
+ 
+void countingSort(int k) {
+    int i, sum, maxi = max(300ll, n);
+    memset(c, 0, sizeof c);
+    for (i = 0; i < n; i++)
+        c[i + k < n ? RA[i + k] : 0]++;
+    for (i = sum = 0; i < maxi; i++) {
+        int t = c[i]; c[i] = sum; sum += t;
+    }
+    for (i = 0; i < n; i++)
+        tempSA[c[SA[i] + k < n ? RA[SA[i] + k] : 0]++] = SA[i];
+    for (i = 0; i < n; i++)
+        SA[i] = tempSA[i];
+}
+ 
+void constructSA() {
+    int i, k, r;
+    for (i = 0; i < n; i++) RA[i] = T[i];
+    for (i = 0; i < n; i++) SA[i] = i;
+    for (k = 1; k < n; k <<= 1) {
+        countingSort(k);
+        countingSort(0);
+        tempRA[SA[0]] = r = 0;
+        for (i = 1; i < n; i++)
+            tempRA[SA[i]] = (RA[SA[i]] == RA[SA[i-1]] && RA[SA[i] + k] == RA[SA[i-1] + k]) ? r : ++r;
+        for (i = 0; i < n; i++)
+            RA[i] = tempRA[i];
+        if (RA[SA[n-1]] == n-1) break;
+    }
+}
+ 
+void constructLCP() {
+    int i, L;
+    Phi[SA[0]] = -1;
+    for (i = 1; i < n; i++)
+        Phi[SA[i]] = SA[i-1];
+    for (i = L = 0; i < n; i++) {
+        if (Phi[i] == -1) {
+            PLCP[i] = 0;
+            continue;
+        }
+        while (T[i + L] == T[Phi[i] + L]) L++;
+        PLCP[i] = L;
+        L = max(L - 1, 0ll);
+    }
+    for (i = 0; i < n; i++)
+        LCP[i] = PLCP[SA[i]];
+}
+ 
+void solve(int xyz)
+{
+  string input;
+    getline(cin, input);
+    strcpy(T, input.c_str());
+    n = (int)strlen(T);
+    T[n++] = '$';
+    constructSA();
+    constructLCP();
+    int k;cin>>k;
+    vector<int>ans(n+1);
+    REP(i,1,n){
+        int start=LCP[i]+1;
+        int end=n-SA[i];
+        ans[start]++;ans[end]--;
+    }
+    REP(i,2,n){ans[i]+=ans[i-1];}
+    REP(i,1,n){cout<<ans[i]<<" ";}
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
