0464f6acc002098cd50fea5e106ed549f16f6622
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 14:33:48 +0530

    Create Playlist.cpp

---
+++ sorting and searching/Playlist.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+ 
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+signed main(){
+    int n; cin>>n;
+    vector<int>v(n);
+    REP(i,0,n) cin>>v[i];
+    int i=0,j=0;
+    set<int>s;
+    int ans=0;
+    while(i<n and j<n){
+        if(j<n and !s.count(v[j])){
+            s.insert(v[j]);
+            ans=max(ans,j-i+1);
+            j++;
+        }
+        if(j<n and s.count(v[j])){
+            s.erase(v[i]);
+            i++;
+        }
+    }
+    cout<<ans;
+}
