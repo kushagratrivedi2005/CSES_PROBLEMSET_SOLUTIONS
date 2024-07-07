15e8f5657c893398ee9f9018479051e8a8647960
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 15:01:46 +0530

    Create Hamiltonian Flights.cpp

---
+++ Graph Algorithms/Hamiltonian Flights.cpp
@@ -0,0 +1,69 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+ 
+int dp[1<<20][20];
+ 
+void solve(int xyz)
+{
+    int n,k;cin>>n>>k;
+    vector<vector<int>>graph(n+1);
+    REP(i,0,k){
+      int x,y;cin>>x>>y;
+      graph[y].pb(x);
+    }
+    dp[1][1]=1;
+    for(int s=2;s<1<<n;s++){
+      if((s&1)==0)continue;
+      for(int end=1;end<=n;end++){
+        if(!(s&(1<<(end-1))))continue;
+        int x=s-(1<<(end-1));
+        for(int i:graph[end]){
+          if(s&(1<<(i-1))){
+            dp[s][end]+=dp[x][i];
+            dp[s][end]%=mod;
+          }
+        }
+      }
+    }
+    cout<<dp[(1<<n)-1][n]<<endl;return;
+}
+ 
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
