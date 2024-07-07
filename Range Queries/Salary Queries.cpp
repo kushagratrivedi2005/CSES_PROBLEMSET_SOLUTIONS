45d4a2774a02caa50897bfe2b0ba3327afbcc138
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 15:06:35 +0530

    Create Salary Queries.cpp

---
+++ Range Queries/Salary Queries.cpp
@@ -0,0 +1,64 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+ 
+ 
+void solve(int xyz)
+{
+    int n,m;cin>>n>>m;vector<int>values(n+1);REP(i,1,n+1)cin>>values[i];
+    oset<pair<int,int>>check;
+    REP(i,1,n+1){
+        check.insert({values[i],i});
+    }
+    while(m--){
+        char c;cin>>c;
+        if(c=='?'){
+            int x,y;cin>>x>>y;
+            cout<<check.order_of_key({y,1000000})-check.order_of_key({x-1,1000000})<<endl;
+        }
+        else{
+            int x,y;cin>>x>>y;
+            check.erase({values[x],x});
+            values[x]=y;
+            check.insert({values[x],x});
+        }
+    }
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
