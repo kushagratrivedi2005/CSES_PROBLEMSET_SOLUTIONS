61cf967f3d05c26dd9033a1c80ef172797443519
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 14:34:07 +0530

    Create Towers.cpp

---
+++ sorting and searching/Towers.cpp
@@ -0,0 +1,42 @@
+
+#include<bits/stdc++.h>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+ 
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+signed main(){
+    int n; cin>>n;
+    multiset<int>s;
+    REP(i,0,n){
+        int x;cin>>x;
+        auto it=s.upper_bound(x);
+        if(it==s.end()){
+            s.insert(x);
+        }
+        else{
+            s.erase(it);
+            s.insert(x);
+        }
+    }
+    cout<<s.size();
+    return 0;
+}
