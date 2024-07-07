fa54d127b3164e69c27399e9d0d46bc29a5cea31
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:09:48 +0530

    Create Cyclic Array.cpp

---
+++ Additional Problems/Cyclic Array.cpp
@@ -0,0 +1,115 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+// int addmod(int x,int y){
+//     return (x+y)%mod;
+// }
+ 
+// int multmod(int x,int y){
+//     return (x*y)%mod;
+// }
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+int countfromhere(int x,int k,vector<int>&jump,int n){
+    if(x>n)x-=n;
+    int curr=x;
+    int arrs=0; 
+    while(curr<x+n){
+        arrs++;curr=jump[curr];
+    }
+    return arrs;
+}
+void solve(int xyz)
+{
+int n,k;cin>>n>>k;vector<int>values(2*n);REP(i,0,n){cin>>values[i];values[i+n]=values[i];}vector<int>prefix(2*n);prefix[0]=values[0];REP(i,1,2*n)prefix[i]=values[i]+prefix[i-1];
+vector<int>length(2*n);vector<int>jump(2*n);
+if(prefix[n-1]<=k){cout<<1<<endl;return;}
+jump[0]=upper_bound(prefix.begin(),prefix.begin()+2*n,k)-prefix.begin();
+REP(i,1,2*n){ 
+    jump[i]=upper_bound(prefix.begin(),prefix.begin()+2*n,k+prefix[i-1])-prefix.begin();
+}
+int index=0;
+REP(i,0,n){
+    int jum=jump[i];
+    length[i]=jum-i;if(length[i]<length[index])index=i;
+}
+int ans=n;
+for(int i=0;i<=length[index];i++){
+ans=min(ans,countfromhere(index+i,k,jump,n));
+}
+cout<<ans<<endl;  
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
