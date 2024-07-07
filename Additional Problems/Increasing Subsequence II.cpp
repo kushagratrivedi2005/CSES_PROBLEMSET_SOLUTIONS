aed5338b59d24ad9a5bbae283d323dcc0240f8b0
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:07:30 +0530

    Create Increasing Subsequence II.cpp

---
+++ Additional Problems/Increasing Subsequence II.cpp
@@ -0,0 +1,123 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+int addmod(int x,int y){
+    return (x+y)%mod;
+}
+ 
+int multmod(int x,int y){
+    return (x*y)%mod;
+}
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+int n;int BIT[200009];
+void update(int i,int val){
+    for(;i<=n;i+=i&(-i)){
+        BIT[i]=addmod(BIT[i],val);
+    }
+}
+int query(int i){
+    int ans=0;
+    for(;i>0;i-=i&(-i)){
+        ans=addmod(ans,BIT[i]);
+    }
+    return ans;
+}
+ 
+void solve(int xyz)
+{
+    cin>>n;vector<int>values(n);REP(i,0,n){cin>>values[i];}
+    map<int,int>mapping;
+    vector<int>copy=values;
+    sort(copy.begin(),copy.end());
+    int start=0;
+    REP(i,0,n){
+        if(mapping.find(copy[i])==mapping.end()){
+            mapping[copy[i]]=++start;
+        }
+    }
+    REP(i,0,n){
+        values[i]=mapping[values[i]];
+    }
+    int ans=0;
+    REP(i,0,n){
+        int q=query(values[i]-1)+1;
+        ans=addmod(ans,q);
+        update(values[i],q);
+    }
+    cout<<ans<<endl;
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
