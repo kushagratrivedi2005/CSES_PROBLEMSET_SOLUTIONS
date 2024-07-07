dd25b2ef27318ff81de17cd0b931a07d10153cd8
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 14:46:17 +0530

    Create Increasing Subsequence.cpp

---
+++ dynamic programming/Increasing Subsequence.cpp
@@ -0,0 +1,55 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+ 
+ 
+void solve(int xyz)
+{
+  int n;cin>>n;vector<int>values(n); REP(i,0,n)cin>>values[i];
+  vector<int>dp;
+  REP(i,0,n){
+    if(dp.size()==0 || dp[dp.size()-1]<values[i]){dp.pb(values[i]);continue;}
+    int index=lower_bound(dp.begin(),dp.end(),values[i])-dp.begin();
+    dp[index]=values[i];
+  }
+  cout<<dp.size(); return;
+}
+ 
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
