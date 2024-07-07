b4ae2fbfe1a6a7cfcdbb9cb29a17f5d73a2c07f6
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:08:30 +0530

    Create Writing Numbers.cpp

---
+++ Additional Problems/Writing Numbers.cpp
@@ -0,0 +1,115 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+// int addmod(int x,int y){
+//     return (x+y)%mod;
+// }
+ 
+// int multmod(int x,int y){
+//     return (x*y)%mod;
+// }
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+int dp[0];
+int check(int n,int k){
+   int ans=0;
+   int leftpart;int rightpart=0;
+   int power=0;
+   while(n>0){
+    int curr=n%10;leftpart=n/10;
+    if(curr==1){ans+=leftpart*dp[power]+rightpart+1;}
+    if(curr!=1){ans+=(leftpart+(curr!=0))*dp[power];}
+    rightpart+=dp[power]*curr;power++;n/=10;
+   } 
+   return ans<=k;
+}
+ 
+void solve(int xyz)
+{
+    int n;cin>>n;
+dp[0]=1;
+REP(i,1,19){dp[i]=dp[i-1]*10;}
+int l=1;int r=1e18;
+int ans=r;
+while(l<=r){
+    int mid=l+(r-l)/2;
+    int z=check(mid,n);
+    if(!z){r=mid-1;}
+    else{l=mid+1;ans=mid;}
+}
+cout<<ans;
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
