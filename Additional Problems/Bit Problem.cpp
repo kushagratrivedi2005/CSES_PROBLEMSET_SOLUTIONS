2a6f746520a4721bb035196bc947c29dc2c6f9ff
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:10:47 +0530

    Create Bit Problem.cpp

---
+++ Additional Problems/Bit Problem.cpp
@@ -0,0 +1,112 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+// int addmod(int x,int y){
+//     return (x+y)%mod;
+// }
+ 
+// int multmod(int x,int y){
+//     return (x*y)%mod;
+// }
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+const int maxi=1<<20;
+vector<int>freq(maxi);
+int dpzeros[maxi][20]; int dpones[maxi][20];
+ 
+void solve(int xyz)
+{
+int n;cin>>n;vector<int>values(n);REP(i,0,n){cin>>values[i];freq[values[i]]++;}
+REP(i,1,maxi){
+    dpzeros[i][0]=freq[i];
+    if(i&1){dpzeros[i][0]+=freq[i^1];}
+    REP(j,1,20){
+        dpzeros[i][j]+=dpzeros[i][j-1];
+        if(i>>j&1)dpzeros[i][j]+=dpzeros[i^1<<j][j-1];
+    }
+}
+for(int i=maxi-1;i>0;i--){
+    dpones[i][0]=freq[i];
+    if(!(i&1))
+        dpones[i][0]+=freq[i^1];
+        REP(j,1,20){
+            dpones[i][j]=dpones[i][j-1];
+            if(!(i>>j&1))dpones[i][j]+=dpones[i^1<<j][j-1];
+        }
+}
+REP(i,0,n){cout<<dpzeros[values[i]][19]<<" "<<dpones[values[i]][19]<<" "<<n-dpzeros[values[i]^(maxi-1)][19]<<endl;}
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
