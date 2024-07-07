6b14e88736859cf5edc00ae7ef1524e0f76dcb26
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 14:46:55 +0530

    Create Elevator Rides.cpp

---
+++ dynamic programming/Elevator Rides.cpp
@@ -0,0 +1,64 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+ 
+void solve(int xyz)
+{
+    int n,m;cin>>n>>m;
+    vector<int>values(n); REP(i,0,n)cin>>values[i];
+    vector<pair<int,int>>dp(1<<n,{1e9,1e9});
+    dp[0]={1,0};
+    for(int i=1;i<(1<<n);i++){
+        for(int bit=0;bit<n;bit++){
+            if(i&(1<<bit)){
+                int space=dp[i^(1<<bit)].second;
+                if(space+values[bit]<=m){
+                    dp[i]=min(dp[i],{dp[i^(1<<bit)].first,space+values[bit]});
+                }
+                else{
+                    dp[i]=min(dp[i],{dp[i^(1<<bit)].first+1,values[bit]});
+                }
+            }
+        }
+    }
+    cout<<dp[(1<<n)-1].first;
+}
+ 
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
