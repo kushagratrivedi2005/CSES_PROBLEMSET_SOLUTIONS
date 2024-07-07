f7b3304e7e49c0ea80cb4e6f8630a17debab99aa
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 14:36:48 +0530

    Create Tasks and Deadlines.cpp

---
+++ sorting and searching/Tasks and Deadlines.cpp
@@ -0,0 +1,43 @@
+
+#include<bits/stdc++.h>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+ 
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+signed main(){
+    int n;cin>>n;
+    vector<pair<int,int>>values(n);
+    for(int i=0 ; i<n; i++){
+        int x,y; cin>>x>>y;
+        values[i].first=x;
+        values[i].second=y;
+    }
+    sort(values.begin(),values.end());
+    int time=0;
+    int reward=0;
+    for (int i = 0; i < n; i++)
+    {
+        time+=values[i].first;
+        reward+=values[i].second-time;
+    }
+    cout<<reward;
+}
