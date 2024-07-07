3f1d7a649f4ccc305fd56b2006e8d7c94c79ab77
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 14:46:01 +0530

    Create Two Sets II.cpp

---
+++ dynamic programming/Two Sets II.cpp
@@ -0,0 +1,58 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+int dp[250001][501];
+void solve(int xyz)
+{
+    int n;cin>>n;
+    int x=(n)*(n+1)/2;
+    if(x&1){cout<<0<<endl;return;}
+    else{x/=2;}
+    dp[0][0]=1;
+    for(int i=0;i<=x;i++){
+        for(int j=1;j<n;j++){
+            dp[i][j]=dp[i][j-1]%mod;
+            if(i>=j)dp[i][j]+=dp[i-j][j-1]%mod;
+        }
+    }
+    cout<<dp[x][n-1]%mod<<endl;return;
+}
+ 
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
