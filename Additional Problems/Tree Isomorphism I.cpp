e1e53f42da70d0e3f6442384694da3098e5812b9
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:11:33 +0530

    Create Tree Isomorphism I.cpp

---
+++ Additional Problems/Tree Isomorphism I.cpp
@@ -0,0 +1,114 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+// int addmod(int x,int y){
+//     return (x+y)%mod;
+// }
+ 
+// int multmod(int x,int y){
+//     return (x*y)%mod;
+// }
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+map<vector<int>,int>treetype;
+int type=0;
+int dfs(int x,int par,vector<vector<int>>&graph){
+    vector<int>vec;
+    for(auto i:graph[x]){
+        if(i!=par)
+        vec.pb(dfs(i,x,graph));
+    }
+    sort(vec.begin(),vec.end());
+    if(!treetype.count(vec))treetype[vec]=type++;
+    return treetype[vec];
+}
+ 
+void solve(int xyz)
+{
+    int n;cin>>n;
+    vector<vector<int>>graph1(n+1);vector<vector<int>>graph2(n+1);
+    REP(i,0,n-1){
+        int x,y;cin>>x>>y;graph1[x].pb(y);graph1[y].pb(x);
+    }
+    REP(i,0,n-1){
+        int x,y;cin>>x>>y;graph2[x].pb(y);graph2[y].pb(x);
+    }
+    int ans1=dfs(1,-1,graph1);int ans2=dfs(1,-1,graph2);
+    // cout<<ans1<<" "<<ans2<<endl;
+    if(ans1==ans2){cout<<"YES"<<endl;return;}
+    else{cout<<"NO"<<endl;return;}
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  int t;cin>>t;
+  REP(xyz,0,t){
+    solve(xyz);
+  }
+}
