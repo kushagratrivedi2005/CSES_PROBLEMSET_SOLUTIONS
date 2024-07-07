4d044eb949218da5379e3216e30ccd8ec893c0b4
kushagratrivedi2005 <152433706+kushagratrivedi2005@users.noreply.github.com>
2024-07-07 16:10:03 +0530

    Create List of Sums.cpp

---
+++ Additional Problems/List of Sums.cpp
@@ -0,0 +1,107 @@
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007 
+#define int long long
+#define REP(i,m,n) for(int i=m;i<n;i++)
+#define pb push_back
+#define ff first
+#define ss second
+#define vi vector<int>
+#define ii pair<int,int>
+#define mp make_pair
+#define vii vector<ii>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using oset= tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // find_by_order, order_of_key
+ 
+ 
+ 
+ 
+// const int n=2e5+5;
+// int power(int a,int b){
+//     if(b==0)return 1;
+//     if(a==0)return 0;
+//     if(a==1)return 1;
+//     int ans=1;
+//     a=a%mod;
+//     while(b>0){
+//         if(b&1){
+//             ans=ans*a%mod;
+//         }
+//         b>>=1;
+//         a=(a*a)%mod;
+//     }
+//     return ans;
+// }
+// int fact[n]; int inv[n];
+// void once(){
+//     fact[0]=1;
+//     REP(i,1,n){
+//         fact[i]=fact[i-1]*i%mod;
+//     }
+//     inv[0]=1;
+//     REP(i,1,n){
+//         inv[i]=power(fact[i],mod-2)%mod;
+//     }
+// }
+// int ncr(int n,int r){
+//     int ans=fact[n]%mod;
+//     ans*=inv[r]%mod;
+//     ans%=mod;
+//     ans*=inv[n-r]%mod;
+//     ans%=mod;
+//     return ans;
+// }
+// int addmod(int x,int y){
+//     return (x+y)%mod;
+// }
+ 
+// int multmod(int x,int y){
+//     return (x*y)%mod;
+// }
+/*
+code BY-
+ █ ▄▀  █  █  █▀▀▀█  █  █  █▀▀█  █▀▀█  █▀▀█  █▀▀█ 　 
+ █▀▄   █  █  ▀▀▀▄▄  █▀▀█  █▄▄█  █ ▄▄  █▄▄▀  █▄▄█ 　 
+ █  █  ▀▄▄▀  █▄▄▄█  █  █  █  █  █▄▄█  █  █  █  █ 　 
+ 
+▀▀█▀▀ █▀▀█ ▀█▀  █   █  █▀▀▀ █▀▀▄ ▀█▀ 
+  █   █▄▄▀  █    █ █   █▀▀▀ █  █  █
+  █   █  █ ▄█▄   ▀▄▀   █▄▄▄ █▄▄▀ ▄█▄ 
+*/
+int n,posspairs;multiset<int>sumpairs;
+vector<int>values(101);vector<int>pairsum(4951);
+bool check(int x){
+    values[0]=x;
+    sumpairs.clear();
+    REP(i,0,posspairs){sumpairs.insert(pairsum[i]);}
+    REP(i,1,n){REP(j,0,i-1){if(sumpairs.find(values[j]+values[i-1])==sumpairs.end())return false;sumpairs.erase(sumpairs.find(values[i-1]+values[j]));}values[i]=*sumpairs.begin()-values[0];}
+    REP(j,0,n-1){if(sumpairs.find(values[j]+values[n-1])==sumpairs.end())return false;sumpairs.erase(sumpairs.find(values[n-1]+values[j]));}
+    return true;
+}
+ 
+void solve(int xyz)
+{
+    cin>>n;posspairs=(n*(n-1))/2;REP(i,0,posspairs){cin>>pairsum[i];}sort(pairsum.begin(),pairsum.begin()+posspairs);
+    REP(i,1,posspairs){
+        int firstval=(pairsum[0]+pairsum[1]-pairsum[i])/2;
+        if(0<firstval && check(firstval)){
+            REP(j,0,n)cout<<values[j]<<" ";return;
+        }
+    }
+}
+ 
+signed main(){
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+  
+ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//   int t;cin>>t;
+  REP(xyz,0,1){
+    solve(xyz);
+  }
+}
